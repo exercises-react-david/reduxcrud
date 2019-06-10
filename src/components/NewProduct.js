4d8563cb4812d30391d57a8ea75bfdf1f11125b8
David Morales <davidmhdez22@gmail.com>
2019-06-10 16:57:06 -0600

    commit 2

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -2,6 +2,30 @@ import React,{Component} from 'react';
 
 class NewProduct extends Component {
 
+    state={
+        product:{},
+        error: false
+    }
+
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+
+    createProduct = (e) =>{
+        e.preventDefault();
+
+        const newProduct = {
+            name: this.nameRef.current.value,
+            price: this.priceRef.current.value
+        };
+
+        this.setState({
+            product: newProduct
+        });
+
+        console.log(this.state.product);
+
+    }
+
     render() {
         return (
             <div className="row justify-content-center mt-5">
@@ -9,14 +33,14 @@ class NewProduct extends Component {
                     <div className="card">
                         <div className="card-body">
                             <h2 className="text-center">Agregar Nuevo Producto</h2>
-                                <form>
+                                <form onSubmit={this.createProduct}>
                                     <div className="form-group">
                                         <label>Titulo</label>
-                                        <input type="text" className="form-control" placeholder="Titulo" />
+                                        <input ref={this.nameRef} type="text" className="form-control" placeholder="Titulo" />
                                     </div>
                                     <div className="form-group">
                                         <label>Precio del Producto</label>
-                                        <input type="text" className="form-control" placeholder="Precio" />
+                                        <input ref={this.priceRef} type="text" className="form-control" placeholder="Precio" />
                                     </div>
                                     <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">Agregar</button>
                                 </form>
